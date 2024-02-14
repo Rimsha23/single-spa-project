c5394338c80a254f1c78cc4829729d438fb88013
Rimsha23 <imrimshamalik2023@gmail.com>
2024-02-14 17:26:44 +0500

    initial commit

--- react-multiples/src/App.tsx
+++ react-multiples/src/App.tsx
@@ -1,4 +1,4 @@
-cd import { Link, BrowserRouter, Route, Routes } from "react-router-dom";
+import { Link, BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
